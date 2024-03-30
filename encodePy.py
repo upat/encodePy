8d0a7fa01fb0240718a9c7595e78d546db0a2e2c
upat <18456125+upat@users.noreply.github.com>
2024-03-31 03:55:59 +0900

    多重化が原因なのか時々AAC部分のデコードができずエラーになるため、エラー時はffmpegで先頭1sを映像ごと切り取ってエンコードするようにした。その他ログ出力やエラー判定周りを微修正。

--- encodePy.py
+++ encodePy.py
@@ -4,7 +4,14 @@ from pathlib import Path
 from py_func.def_const import DefineConst
 from py_func.read_log import *
 
+# 強制終了時にコールする
+def force_exit():
+	subprocess.run( 'pause', shell=True ) # pauseコマンド
+	sys.exit()
+
 dc = DefineConst( sys.argv[1] )
+if not dc.init_result:
+	force_exit()
 
 # 最終出力先のディレクトリを作成
 if not Path( dc.outfile_path ).exists():
@@ -16,14 +23,26 @@ read_log( dc.srcfile_path, dc.outfile_path, dc.srcfile_filename )
 enc_res = subprocess.Popen( dc.enc_cmd(), encoding='utf8', stdout=subprocess.PIPE )
 # subprocess.Popenの処理終了まで待ち
 enc_res.wait()
-# エラー終了した場合は強制終了
+# エラー終了した場合はリトライ
 if enc_res.returncode != 0:
-	print( 'Failed Encode Process.' )
-	sys.exit()
+	print( 'Retry Encode Process.' )
+	# shell=Trueでゴリ押しする苦肉の策(Popenを2つ立ち上げてパイプすると非同期のせいかffmpegがNVEncを置き去りにして勝手に終了するため)
+	enc_retry_res = subprocess.Popen( ' '.join( dc.enc_cmd( True ) ), encoding='utf8', stdout=subprocess.PIPE, shell=True )
+	enc_retry_res.wait() # 待ち
+	
+	if enc_retry_res.returncode != 0:
+		# リトライもエラー終了した場合終了
+		print( 'Failed Encode Process.' )
+		force_exit()
+	else:
+		# リトライして正常終了できた場合は空ファイル作成
+		Path( dc.outfile_path, dc.srcfile_filename + '__retry.txt' ).touch()
 
 # ローカルサーバー稼働中 and 出力済みファイル有
 if Path( dc.media_server ).exists() and Path( dc.outfile_fullpath ).exists(): # 
 	# # ローカルサーバーへコピー
 	if not Path( dc.copyfile_path ).exists():
 		Path( dc.copyfile_path ).mkdir( parents=True )
-	shutil.copy2( dc.outfile_fullpath, dc.copyfile_fullpath )
+	print( 'Start Backup Copy...' )
+	shutil.copy2( dc.outfile_fullpath, dc.copyfile_fullpath ) # 上書きコピー
+	print( 'Complete Backup Copy.' )
--- py_func/def_const.py
+++ py_func/def_const.py
@@ -1,39 +1,51 @@
 # encoding: utf-8
-import cv2, sys
+import cv2
 from pathlib import Path
 
+# エンコードに必要な入出力ファイルパス、コマンドオプションなどの定数管理を行うクラス
 class DefineConst:
 	# NVEncエンコーダー
 	_nvencoder = r''
+	# ffmpeg
+	_ffmpeg = r''
 	# rff処理を行う入力ファイル文字列
 	_rffstr = [ '' ]
 	# ファイル保存先(この後に入力ファイルの階層が続く)
 	_sav_dir = r''
 	# バックアップコピー保存場所
 	media_server = r''
+	# init処理結果
+	init_result = True
 
 	# 初期化
 	def __init__( self, input_filepath ):
-		if not Path( input_filepath ).exists() or input_filepath == '': # if not ～ orはnot直後の条件にのみ有効
+		# パスが絶対パスではない(空の文字列含む)場合
+		if not Path( input_filepath ).is_absolute():
+			print( 'Not Absolute Path.' )
+			DefineConst.init_result = False
+		# 入力ファイルが存在しない場合
+		elif not Path( input_filepath ).exists():
 			print( 'Not Found Input File.' )
-			sys.exit() # 入力ファイルが存在しない場合強制終了
+			DefineConst.init_result = False
+		# 出力先ストレージが存在しない場合
 		elif not Path( DefineConst._sav_dir[0:3] ).exists():
 			print( 'Not Found Output Directory.' )
-			sys.exit() # 出力先ストレージが存在しない場合強制終了
+			DefineConst.init_result = False
 
-		self.srcfile_fullpath = input_filepath                       # フルパス取得
-		self.srcfile_path     = Path( self.srcfile_fullpath ).parent # フルパスからファイル名無しパスを取得
-		self.srcfile_filename = Path( self.srcfile_fullpath ).stem   # フルパスから拡張子無しファイル名を取得
-		self.outfile_dirname  = Path( self.srcfile_path ).name       # ファイル名無しパスの末尾のフォルダ名を取得
-		
-		self.outfile_path  = Path( DefineConst._sav_dir, self.outfile_dirname )     # 出力パス(ファイル名なし)を作成
-		self.copyfile_path = Path( DefineConst.media_server, self.outfile_dirname ) # バックアップコピー先のパス(ファイル名なし)を作成
-		
-		self.outfile_fullpath  = Path( self.outfile_path, self.srcfile_filename + '.mp4' )  # 出力パスを作成
-		self.copyfile_fullpath = Path( self.copyfile_path, self.srcfile_filename + '.mp4' ) # バックアップコピー先のパスを作成
+		if DefineConst.init_result:
+			self.srcfile_fullpath = input_filepath                       # フルパス取得
+			self.srcfile_path     = Path( self.srcfile_fullpath ).parent # フルパスからファイル名無しパスを取得
+			self.srcfile_filename = Path( self.srcfile_fullpath ).stem   # フルパスから拡張子無しファイル名を取得
+			self.outfile_dirname  = Path( self.srcfile_path ).name       # ファイル名無しパスの末尾のフォルダ名を取得
+			
+			self.outfile_path  = Path( DefineConst._sav_dir, self.outfile_dirname )     # 出力パス(ファイル名なし)を作成
+			self.copyfile_path = Path( DefineConst.media_server, self.outfile_dirname ) # バックアップコピー先のパス(ファイル名なし)を作成
+			
+			self.outfile_fullpath  = Path( self.outfile_path, self.srcfile_filename + '.mp4' )  # 出力パスを作成
+			self.copyfile_fullpath = Path( self.copyfile_path, self.srcfile_filename + '.mp4' ) # バックアップコピー先のパスを作成
 		
 	# エンコーダーで使用するオプション文字列リストの生成
-	def enc_cmd( self ):
+	def enc_cmd( self, _retry = False ):
 		# python-opencvで映像サイズ読み出し
 		_video_capture = cv2.VideoCapture( self.srcfile_fullpath )
 		_video_height = _video_capture.get( cv2.CAP_PROP_FRAME_HEIGHT )
@@ -48,44 +60,46 @@ class DefineConst:
 			# fHD(1080)未満ならとりあえずそのまま
 			_video_size = str( _video_width ) + 'x' + str( _video_height )
 		
-		if self._check_rff(): # rffフラグ判定
-			# rffフラグあり
-			cmd_str = [ DefineConst._nvencoder,
-						'--avhw',
-						'--audio-copy',
-						'--interlace', 'tff',
-						'--vpp-afs', 'preset=anime,rff=true',
-						'--codec', 'hevc',
-						'--vbrhq', '0',
-						'--vbr-quality', '27',
-						'--preset', 'quality',
-						'--lookahead', '32',
-						'--gop-len', '300',
-						'--bref-mode', 'each',
-						'--videoformat', 'ntsc',
-						'--vpp-resize', 'spline64',
-						'--output-res', _video_size,
-						'-i', self.srcfile_fullpath,
-						'-o', str( self.outfile_fullpath ) ]
+		# コマンドオプション用リスト
+		cmd_str = [ DefineConst._nvencoder,
+					'--avhw',
+					'--audio-copy',
+					'--interlace', 'tff',
+					'--codec', 'hevc',
+					'--vbrhq', '0',
+					'--vbr-quality', '27',
+					'--preset', 'quality',
+					'--lookahead', '32',
+					'--gop-len', '300',
+					'--bref-mode', 'each',
+					'--videoformat', 'ntsc',
+					'--vpp-resize', 'spline64',
+					'--output-res', _video_size ]
+
+		# rffフラグ判定(コマンドオプション追加)
+		if self._check_rff():
+			# rffフラグあり用のインターレース解除
+			cmd_str += [ '--vpp-afs', 'preset=anime,rff=true' ]
 		else:
-			# rffフラグなし
-			cmd_str = [ DefineConst._nvencoder,
-						'--avhw',
-						'--audio-copy',
-						'--interlace', 'tff',
-						'--vpp-deinterlace', 'normal',
-						'--codec', 'hevc',
-						'--vbrhq', '0',
-						'--vbr-quality', '27',
-						'--preset', 'quality',
-						'--lookahead', '32',
-						'--gop-len', '300',
-						'--bref-mode', 'each',
-						'--videoformat', 'ntsc',
-						'--vpp-resize', 'spline64',
-						'--output-res', _video_size,
-						'--log-framelist', 'debug_framelist.csv', # デバッグ出力(repeatに2以上の値があればrff)
-						'-i', self.srcfile_fullpath,
+			# rffフラグなし(通常)用のインターレース解除+フレームログ出力
+			cmd_str += [ '--vpp-deinterlace', 'normal',
+						'--log-framelist', 'debug_framelist.csv' ] # デバッグ出力(repeatに2以上の値があればrff)
+		
+		# エンコードをリトライする判定(入出力コマンドオプション追加+ffmpegコマンド追加)
+		if _retry:
+			# ffmpegからのパイプ入力+出力
+			cmd_str += [ '-i', '-' ,
+						'-o', '"' + str( self.outfile_fullpath ) + '"' ] # ダブルクォーテーションはshell=Trueで使うため
+			# ffmpegからパイプ入力するため、NVEncのコマンドを後ろにする
+			cmd_str = [ DefineConst._ffmpeg,
+						'-i', '"' + str( self.srcfile_fullpath ) + '"',  # ダブルクォーテーションはshell=Trueで使うため
+						'-ss', '1',                                      # 先頭の映像を1sを削る
+						'-c', 'copy',
+						'-f', 'mpegts',
+						'-', '|' ] + cmd_str
+		else:
+			# 通常入力+出力
+			cmd_str += [ '-i', str( self.srcfile_fullpath ),
 						'-o', str( self.outfile_fullpath ) ]
 		return cmd_str
 
